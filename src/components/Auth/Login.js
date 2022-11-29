59416827920a3857316fce8a1f1e9a5034119c7b
Matthew Greco <mgreco2112@tutanota.com>
2022-11-29 16:29:10 -0500

    created and successfully tested login

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,9 +1,64 @@
-import React, {useState, useEffect} from "react";
-import LoginForm from "./LoginForm"
+import React, {useState, useEffect, useContext} from "react";
+import axios from "axios";
+import {AuthContext} from "../poviders/AuthProvider";
+import Container from "../common/Container";
+import { useNavigate } from "react-router-dom";
+import LoginForm from "./LoginForm";
 
 const Login = () => {
+    const navigate = useNavigate();
+
+    const [currentUser, setCurrentUser] = useState({
+        username: "",
+        password: ""
+    });
+
+    const [auth, setAuth] = useContext(AuthContext);
+
+    const updateForm = (field, value) => {
+        setCurrentUser({
+            ...currentUser,
+            [field]: value
+        });
+    }
+
+    const onSubmit = () => {
+        const data = currentUser;
+
+        _loginUser(data);
+    }
+
+    const _loginUser = async (data) => {
+        try {
+            const res = await axios.post("http://localhost:8080/auth/signin", data);
+
+            setAuth({
+                token: res.data.token,
+                profile: {
+                    id: res.data.id,
+                    username: res.data.username
+                },
+                roles: res.data.roles
+            });
+
+            console.log(res.data);
+
+            navigate("/");
+        } catch (err) {
+            console.error(err.message ? err.message : err.response);
+        }
+    }
+
     return (
-        <h1>Login</h1>
+        <Container>
+            <h1>Login</h1>
+
+            <LoginForm
+                currentUser={currentUser}
+                onChange={updateForm}
+                onSubmit={onSubmit}
+            />
+        </Container>
     )
 }
 
