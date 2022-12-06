c9fbb6df36b67e524625250f3981eaf893fc9a99
Matthew Greco <mgreco2112@tutanota.com>
2022-12-06 13:01:29 -0500

    built PostNewService functions to interact with form, created NewServiceForm

--- src/components/Services/NewServiceForm.js
+++ src/components/Services/NewServiceForm.js
@@ -1,27 +1,123 @@
 import React from "react";
+import Container from "../common/Container";
 import Form from "../common/Form";
 import Button from "../common/Button";
 import Input from "../common/Input";
 import InlineInputContainer from "../common/InlineInputContainer";
-import Radio from "../common/Radio";
+import Checkbox from "../common/Checkbox";
 
-/**
- * 
- * private String name;
-    private boolean isExternallyFunded;
-    private boolean isFunded;
-    private String date;
-    private String fName;
-    private String lName;
-    private String address;
-    private String city;
-    private String gender;
-    private Integer age;
- */
 
 const NewServiceForm = (props) => {
+
+    const {onSubmit, onChange, newService} = props;
+
+    const handleChange = (e) => {
+        onChange(e.target.id, e.target.value);
+    }
+
+    const handleCheckbox = () => {
+        const box = document.getElementById("isExternallyFunded");
+
+        if (box.checked) {
+            newService.isExternallyFunded = true;
+        } else {
+            newService.isExternallyFunded = false;
+        }
+    }
+
     return (
-        <h1>Form</h1>
+        <Container>
+            <Form onSubmit={onSubmit} style={{marginTop: '1em'}}>
+                <InlineInputContainer>
+                    <Input
+                        name="name"
+                        id="name"
+                        value={newService.name}
+                        placeholder="Service Name"
+                        onChange={handleChange}
+                        required
+                    />
+                </InlineInputContainer>
+                <InlineInputContainer>
+                    <Input
+                        name="name"
+                        id="name"
+                        value={newService.name}
+                        placeholder="Service Name"
+                        onChange={handleChange}
+                        required
+                    />
+                </InlineInputContainer>
+                <InlineInputContainer>
+                    <Input
+                        name="date"
+                        id="date"
+                        value={newService.date}
+                        placeholder="Service Date"
+                        onChange={handleChange}
+                        required
+                    />
+                </InlineInputContainer>
+                <InlineInputContainer>
+                    <Input
+                        name="fName"
+                        id="fName"
+                        value={newService.fName}
+                        placeholder="First Name"
+                        onChange={handleChange}
+                        required
+                    />
+                    <Input
+                        name="lName"
+                        id="lName"
+                        value={newService.lName}
+                        placeholder="Last Name"
+                        onChange={handleChange}
+                        required
+                    />
+                </InlineInputContainer>
+                <InlineInputContainer>
+                    <Input
+                        name="address"
+                        id="address"
+                        value={newService.address}
+                        placeholder="Street Address"
+                        onChange={handleChange}
+                        required
+                    />
+                    <Input
+                        name="city"
+                        id="city"
+                        value={newService.city}
+                        placeholder="City"
+                        onChange={handleChange}
+                        required
+                    />
+                </InlineInputContainer>
+                <InlineInputContainer>
+                    <Input
+                        name="gender"
+                        id="gender"
+                        value={newService.gender}
+                        placeholder="Gender"
+                        onChange={handleChange}
+                        required
+                    />
+                    <Input
+                        name="age"
+                        id="age"
+                        value={newService.age}
+                        placeholder="Age"
+                        onChange={handleChange}
+                        required
+                    />
+                </InlineInputContainer>
+                <InlineInputContainer>
+                    <Checkbox id="isExternallyFunded" name="isExternallyFunded" onChange={handleCheckbox} label="Externally Funded"/>
+                </InlineInputContainer>
+                <Button>Submit</Button>
+            </Form>
+        </Container>
     );
 }
 
--- src/components/Services/PostNewService.js
+++ src/components/Services/PostNewService.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { AuthContext } from "../poviders/AuthProvider";
 import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
+import NewServiceForm from "./NewServiceForm";
 
 const PostNewService = () => {
     const params = useParams();
@@ -11,10 +12,11 @@ const PostNewService = () => {
 
     const [auth] = useContext(AuthContext);
 
+    const [serviceSubmitted, setServiceSubmitted] = useState(true);
+
     const [newService, setNewService] = useState({
        name: "",
        isExternallyFunded: undefined,
-       isFunded: undefined,
        date: "",
        fName: "",
        lName: "",
@@ -24,10 +26,90 @@ const PostNewService = () => {
        age: undefined
     });
 
-    
+    const [newServiceFields, setNewServiceFields] = useState([]);
+
+    const updateNewServiceForm = (field, value) => {
+        setNewService({
+            ...newService,
+            [field]: value
+        });
+    }
+
+    const _postService = async (data) => {
+        try {
+            const res = await axios.post(`http://localhost:8080/service/${params.companyId}`, data, {
+                headers: {
+                    Authorization: `Bearer ${auth.token}`
+                }
+            });
+
+            _postNewServiceFields(newServiceFields, res.data.id);
+        } catch (err) {
+            console.error(err.message ? err.message : err.response);
+        }
+    }
+
+    const _postNewServiceFields = async (data, serviceId) => {
+        let route = `http://localhost:8080/service/customServiceField`;
+
+        if (data.length > 0) {
+            route += `/bulk`;
+        } else {
+
+            if (data[0].isString) {
+                route += `/string`;
+            } else if (data[0].isInt) {
+                route += `/integer`;
+            } else {
+                route += `/float`;
+            }
+
+            data = data[0];
+        }
+        
+
+        route += `/${serviceId}`;
+
+        try {
+            const res = await axios.post(route, data, {
+                headers: {
+                    Authorization: `Bearer ${auth.token}`
+                }
+            });
+
+
+        } catch (err) {
+            console.error(err.message ? err.message : err.response);
+        }
+    }
+
+    const onServiceFormSubmit = () => {
+        setServiceSubmitted(true);
+    }
+
+    const displayForms = () => {
+        if (!serviceSubmitted) {
+            displayNewServiceForm();
+        } else {
+
+        }
+    }
+
+    const displayNewServiceForm = () => {
+        return (
+            <NewServiceForm
+                onSubmit={onServiceFormSubmit}
+                onChange={updateNewServiceForm}
+                newService={newService}
+            />
+        )
+    }
+
 
     return (
-        <h1>NewService</h1>
+        <Container>
+            {displayForms()}
+        </Container>
     );
 }
 
