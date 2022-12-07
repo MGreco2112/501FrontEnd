3513b31415520519b77284c7161a14e00649e7fb
Matthew Greco <mgreco2112@tutanota.com>
2022-12-07 17:18:41 -0500

    created dynamic page for handling display of both parsed service data and pii service data

--- src/components/Services/DisplayService.js
+++ src/components/Services/DisplayService.js
@@ -1,9 +1,7 @@
 import React, {useEffect, useState, useContext} from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import axios from "axios";
 import Container from "../common/Container";
 import { AuthContext } from "../poviders/AuthProvider";
-import EditServiceForm from "../Services/EditServiceForm";
 
 const DisplayService = (props) => {
     const location = useLocation();
@@ -42,9 +40,33 @@ const DisplayService = (props) => {
 
                     <div style={{flexDirection: 'row'}}>
 
-                        <div style={{flexDirection: 'column'}}>
-                            <h2>Funding Date</h2>
+                        <div style={{flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+                            <h2>Funding Date:</h2>
                             <h3>{service.date}</h3>
+                        </div>
+                        {service.fName != null ? 
+                            <div style={{flexDirection: 'column'}}>
+                                <h2>Name:</h2>
+                                <h3>{service.fName}</h3> <h3>{service.lName}</h3>
+                                <h2>Address:</h2> <h2>City:</h2>
+                                <h3>{service.address}</h3> <h3>{service.city}</h3>
+                                <h2>Gender:</h2>
+                                <h3>{service.gender}</h3>
+                            </div>
+                            :
+                            <div/>    
+                        }
+                        <div style={{flexDirection: 'column', paddingLeft: '20px'}}>
+                            {service.funded ?
+                                <h2>Service Funded</h2>
+                                :
+                                <h2>Service Not Funded</h2>
+                            }
+                            {service.isEnternallyFunded ?
+                                <h2>Externally Funded</h2>
+                                :
+                                null
+                            }
                         </div>
                     </div>    
                 </div>
